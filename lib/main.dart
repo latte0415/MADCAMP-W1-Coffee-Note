e2a2319f22dd53db6d491266dad518485345145a
라태형 <laxogud@gmail.com>
2026-01-09 17:21:28 +0900

    modify home page

--- lib/main.dart
+++ lib/main.dart
@@ -63,8 +63,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
 
-      //  home: const MainPage(),
-      home: DbTestPage(),
+       home: const MainPage(),
+      // home: DbTestPage(),
     );
   }
 }
